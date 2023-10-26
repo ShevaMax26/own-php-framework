0d060aa43dcfd9b7435457b28f948385bfcebbb6
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-10-26 14:38:15 +0300

    Added processing "not found" and "method not allowed"

---
+++ framework/Http/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SimplePhpFramework\Http\Exceptions;
+
+class MethodNotAllowedException extends HttpException
+{
+
+}
--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -2,7 +2,10 @@
 
 namespace SimplePhpFramework\Routing;
 
+use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
+use SimplePhpFramework\Http\Exceptions\MethodNotAllowedException;
+use SimplePhpFramework\Http\Exceptions\RouteNotFoundException;
 use SimplePhpFramework\Http\Request;
 use function FastRoute\simpleDispatcher;
 
@@ -10,6 +13,19 @@ class Router implements RouterInterface
 {
 
     public function dispatch(Request $request): array
+    {
+        [$handler, $vars] = $this->extractRouteInfo($request);
+
+        [$controller, $method] = $handler;
+
+        return [[new $controller, $method], $vars];
+    }
+
+    /**
+     * @throws MethodNotAllowedException
+     * @throws RouteNotFoundException
+     */
+    private function extractRouteInfo(Request $request): array
     {
         $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
             $routes = include BASE_PATH . '/routes/web.php';
@@ -19,14 +35,19 @@ class Router implements RouterInterface
             }
         });
 
-
         $routerInfo = $dispatcher->dispatch(
             $request->getMethod(),
             $request->getPath(),
         );
 
-        [$status, [$controller, $method], $vars] = $routerInfo;
-
-        return [[new $controller, $method], $vars];
+        switch ($routerInfo[0]) {
+            case Dispatcher::FOUND:
+                return [$routerInfo[1], $routerInfo[2]];
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = implode(', ', $routerInfo[1]);
+                throw new MethodNotAllowedException("Supported HTTP methods: {$allowedMethods}");
+            default:
+                throw new RouteNotFoundException('Route not found');
+        }
     }
 }
