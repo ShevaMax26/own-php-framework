2b661332109e559ecde52537eb416f236a185055
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-10-31 18:48:50 +0200

    error handling modify

--- config/services.php
+++ config/services.php
@@ -1,6 +1,7 @@
 <?php
 
 use League\Container\Argument\Literal\ArrayArgument;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
 use SimplePhpFramework\Http\Kernel;
@@ -15,6 +16,8 @@ $routes = include BASE_PATH . '/routes/web.php';
 $container = new Container();
 
 $container->delegate(new ReflectionContainer(true));
+
+$container->add('APP_ENV', new StringArgument('production'));
 
 $container->add(RouterInterface::class, Router::class);
 
